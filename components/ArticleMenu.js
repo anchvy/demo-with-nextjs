5f19616dbc8e96239430bbc879e7066d0aa3e02f
anchvy <anchvy@Macintosh.lan>
2019-09-29 22:05:18 +0900

    Add ArticleMenu

---
+++ components/ArticleMenu.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+import PropTypes from 'prop-types'
+import COLORS from '../utils/color'
+import { ON_DESKTOP } from '../utils/style'
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
+const MenuItem = styled.span`
+  color: ${COLORS.DARK_GRAY};
+  cursor: pointer;
+  flex: 0 1 auto;
+  font-size: 20px;
+  margin: 5px;
+  overflow: hidden;
+  padding: 7px;
+  white-space: nowrap;
+
+  ${props =>
+    props.isActive &&
+    css`
+      border-bottom: 2px solid ${COLORS.LIGHT_GREEN};
+      color: ${COLORS.DARK_BLUE};
+      font-style: italic;
+    `}
+
+  ${ON_DESKTOP`
+    padding: 17px;
+  `}
+`
+
+/* -------------------------------------------- *
+ * REACT COMPONENT
+ * -------------------------------------------- */
+
+const ArticleMenu = props => {
+  const { items, onClickMenuItem, activeItem, ...rest } = props
+
+  return (
+    <Wrapper {...rest}>
+      {items.map(item => (
+        <MenuItem key={item} data-item={item} onClick={onClickMenuItem} isActive={activeItem === item}>
+          {item}
+        </MenuItem>
+      ))}
+    </Wrapper>
+  )
+}
+
+ArticleMenu.propTypes = {
+  activeItem: PropTypes.string,
+  items: PropTypes.array,
+  onClickMenuItem: PropTypes.func,
+}
+
+ArticleMenu.defaultProps = {
+  activeItem: '',
+  items: [],
+  onClickMenuItem: () => null,
+}
+
+export default ArticleMenu
--- components/HomePage.js
+++ components/HomePage.js
@@ -1,42 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 import Header from './Header'
 import Container from './styled/Container'
 import ArticleSection from './ArticleSection'
+import ArticleMenu from './ArticleMenu'
 import ShareBox from './ShareBox'
 
 import useData from '../hooks/useData'
 import { ON_DESKTOP } from '../utils/style'
+import COLORS from '../utils/color'
 
 const StyledArticleSection = styled(ArticleSection)`
-  padding: 44px 0;
+  padding: 25px 0;
+  ${ON_DESKTOP`
+    padding: 44px 0;
+  `}
 `
 const StyledContainer = styled(Container)`
   background: white;
   border-radius: 15px 15px 0 0;
+  padding-top: 25px;
   position: relative;
   top: -15px;
 
   ${ON_DESKTOP`
     border-radius: 0;
+    padding-top: 60px;
     padding-bottom: 50px;
     top: 0;
   `}
+`
+const SectionHeader = styled.div`
+  color: ${COLORS.DARK_BLUE};
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+
+  ${ON_DESKTOP`
+    font-size: 36px;
+  `}
+`
+const StyledArticleMenu = styled(ArticleMenu)`
+  margin-top: 15px;
 `
 
 /* -------------------------------------------- *
  * REACT COMPONENT
  * -------------------------------------------- */
 
+const ALL_CATEGORY_ITEM = 'All'
+
 const HomeComponent = () => {
-  const data = useData()
+  const [activeCategoryName, setActiveCategoryName] = useState(ALL_CATEGORY_ITEM)
+  const data = useData(activeCategoryName)
+
+  const onClickMenuItem = event => {
+    const categoryName = event.currentTarget.getAttribute('data-item')
+    setActiveCategoryName(categoryName)
+  }
 
   return (
     <>
       <Header />
       <StyledContainer>
+        <SectionHeader>Recommended</SectionHeader>
+        <StyledArticleMenu
+          activeItem={activeCategoryName}
+          items={[ALL_CATEGORY_ITEM, 'Casad', 'Dasdasd', 'asdasd', 'sdasd']}
+          onClickMenuItem={onClickMenuItem}
+        />
         <StyledArticleSection items={data} title="recommended articles" seeMoreUrl="#" />
+        <SectionHeader>Latest Post</SectionHeader>
         <StyledArticleSection items={data} title="latest posts" seeMoreUrl="#" />
         <ShareBox />
       </StyledContainer>
